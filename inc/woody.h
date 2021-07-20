df67202d867728eac443b8db670f088896c80f5e
Loic BUCKWELL <lfalkau@e2r4p21.42.fr>
2021-07-20 06:17:27 +0200

    end parsing of file header + segment header

--- inc/woody.h
+++ inc/woody.h
@@ -3,4 +3,17 @@
 
 #include <elf.h>
 
+typedef struct
+{
+    Elf64_Ehdr  file_header;
+    Elf64_Phdr  *segments_headers;
+    Elf64_Shdr  *sections_headers;
+    char        *data;
+    size_t      size;
+}   elf64_t;
+
+// int elf64_parse_file_header;
+// int elf64_parse_segment_header;
+// int elf64_parse_section_header;
+
 #endif
--- src/main.c
+++ src/main.c
@@ -18,33 +18,68 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <woody.h>
+
 int ft_error(const char *errmsg)
 {
     write(2, errmsg, strlen(errmsg));
     return (1);
 }
 
-int parse_elf64_file_header(Elf64_Ehdr *header, const char *binary)
+int elf64_parse_file_header(Elf64_Ehdr *header, const char *data, size_t size)
 {
-    memcpy(header->e_ident, binary, EI_NIDENT);
-    if (header->e_ident[0] != 0x7f || memcmp(&(header->e_ident[1]), "ELF", 3) != 0)
+    char expected_ident[] = {0x7f, 'E', 'L', 'F', 2};
+
+    if (size < 0x40)
         return (-1);
-    header->e_entry = *(Elf64_Addr *)(binary + 0x18);
-    header->e_phoff = *(Elf64_Off *)(binary + 0x20);
-    header->e_shoff = *(Elf64_Off *)(binary + 0x28);
-    header->e_ehsize = *(Elf64_Half *)(binary + 0x34);
-    header->e_phentsize = *(Elf64_Half *)(binary + 0x36);
-    header->e_phnum = *(Elf64_Half *)(binary + 0x38);
-    header->e_shentsize = *(Elf64_Half *)(binary + 0x3a);
-    header->e_shnum = *(Elf64_Half *)(binary + 0x3c);
-    header->e_shstrndx = *(Elf64_Half *)(binary + 0x3e);
+    memcpy(header->e_ident, data, EI_NIDENT);
+    if (memcmp(header->e_ident, expected_ident, sizeof expected_ident) != 0)
+        return (-1);
+    header->e_type = *(Elf64_Half *)(data + 0x10);
+    header->e_machine = *(Elf64_Half *)(data + 0x12);
+    header->e_version = *(Elf64_Word *)(data + 0x14);
+    header->e_entry = *(Elf64_Addr *)(data + 0x18);
+    header->e_phoff = *(Elf64_Off *)(data + 0x20);
+    header->e_shoff = *(Elf64_Off *)(data + 0x28);
+    header->e_flags = *(Elf64_Word *)(data + 0x30);
+    header->e_ehsize = *(Elf64_Half *)(data + 0x34);
+    header->e_phentsize = *(Elf64_Half *)(data + 0x36);
+    header->e_phnum = *(Elf64_Half *)(data + 0x38);
+    header->e_shentsize = *(Elf64_Half *)(data + 0x3a);
+    header->e_shnum = *(Elf64_Half *)(data + 0x3c);
+    header->e_shstrndx = *(Elf64_Half *)(data + 0x3e);
     return (0); 
 }
 
-int parse_elf64_program_header(Elf64_Phdr *header, const char *binary, size_t offset)
+int elf64_parse_program_header(Elf64_Phdr *header, const char *data, size_t offset)
 {
-    
+    header->p_type = *(Elf64_Word *)(data + offset + 0x0);
+    header->p_flags = *(Elf64_Word *)(data + offset + 0x04);
+    header->p_offset = *(Elf64_Off *)(data + offset + 0x08);
+    header->p_vaddr = *(Elf64_Addr *)(data + offset + 0x10);
+    header->p_paddr = *(Elf64_Addr *)(data + offset + 0x18);
+    header->p_filesz = *(Elf64_Xword *)(data + offset + 0x20);
+    header->p_memsz = *(Elf64_Xword *)(data + offset + 0x28);
+    header->p_align = *(Elf64_Xword *)(data + offset + 0x30);
     return (0);
+}
+
+int elf64_parse(const char *path, elf64_t *elf)
+{
+    int         fd;
+    struct stat s;
+
+    fd = open(path, O_RDWR);
+	if (fd < 0 || fstat(fd, &s) < 0)
+		return (-1);
+    elf->size = s.st_size;
+    elf->data = malloc(sizeof(char) * elf->size);
+    if (!elf->data)
+        return (-1);
+    if (read(fd, elf->data, elf->size) < 0)
+		return (-1);
+    close(fd);
+    return elf64_parse_file_header(&elf->file_header, elf->data, elf->size);
 }
 
 int xor_encrypt(char *bin, size_t offset, size_t len, const int key)
@@ -56,28 +91,21 @@ int xor_encrypt(char *bin, size_t offset, size_t len, const int key)
     return (0);
 }
 
+void elf64_print(elf64_t *elf)
+{
+    printf("e_ident: %s\n", elf->file_header.e_ident);
+}
+
 int main(int ac, char **av)
 {
-	char			*filename = av[1];
-	char			*binary;
-	int				fd;
-	struct stat		s;
-    Elf64_Ehdr      header = {0};
+	char	    *path = av[1];
+    elf64_t     elf;
 
 	if (ac < 2)
         return ft_error("Usage: ./woody <elf64 executable>\n");
-	fd = open(filename, O_RDWR);
-	if (fd < 0 || fstat(fd, &s) < 0)
-		return ft_error("woody: Error while opening the executable.\n");
-    binary = malloc(sizeof(char) * (s.st_size + 1));
-    if (!binary)
-        return ft_error("woody: Fatal error.\n");
-    if (read(fd, binary, s.st_size) < 0)
-		return ft_error("woody: Error while reading the executable.\n");
-    close(fd);
-    // 0x40 is the expected size for an elf64 header
-    if (s.st_size < 0x40 || parse_elf64_file_header(&header, binary) < 0)
-        return ft_error("woody: The provided file isn't a valid elf64 executable\n");
-
-    return (0);
+    memset(&elf, 0, sizeof(elf64_t));
+    if (elf64_parse(path, &elf) < 0)
+        return ft_error("woody: Failed to parse av[1]. Make sure it does existes and is a valid elf64 file.\n");
+    elf64_print(&elf);
+	return (0);
 }
