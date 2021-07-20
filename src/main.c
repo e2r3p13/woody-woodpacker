fd1ae3cccbfb7967288fd74a1d324e2586b82aa5
Loic BUCKWELL <lfalkau@e2r4p21.42.fr>
2021-07-20 07:31:11 +0200

    debug print function

--- src/main.c
+++ src/main.c
@@ -106,6 +106,9 @@ int elf64_parse(const char *path, elf64_t *elf)
             return (-1);
         elf64_parse_segment(&elf->segments[i], elf->data, elf->header.e_phoff + elf->header.e_phentsize * i);
     }
+    elf->sections = malloc(sizeof(Elf64_Shdr) * elf->header.e_shnum);
+    if (elf->sections == NULL)
+        return (-1);
     for (size_t i = 0; i < elf->header.e_shnum; i++)
     {   
         if (elf->size < elf->header.e_shoff + elf->header.e_shentsize * (i + 1))
@@ -124,9 +127,60 @@ int xor_encrypt(char *bin, size_t offset, size_t len, const int key)
     return (0);
 }
 
-void elf64_print(elf64_t *elf)
+void elf64_print(elf64_t *elf, int flags)
 {
-    printf("e_ident: %s\n", elf->header.e_ident);
+    if (flags & 0b1)
+    {
+        printf("ELF header:\n");
+        printf("\te_ident: %s\n", elf->header.e_ident);
+        printf("\te_type: 0x%x\n", elf->header.e_type);
+        printf("\te_machine: 0x%x\n", elf->header.e_machine);
+        printf("\te_version: 0x%x\n", elf->header.e_version);
+        printf("\te_entry: 0x%lx\n", elf->header.e_entry);
+        printf("\te_phoff: %ld\n", elf->header.e_phoff);
+        printf("\te_shoff: %ld\n", elf->header.e_shoff);
+        printf("\te_flags: 0x%x\n", elf->header.e_flags);
+        printf("\te_ehsize: %d\n", elf->header.e_ehsize);
+        printf("\te_phentsize: %d\n", elf->header.e_phentsize);
+        printf("\te_phnum: %d\n", elf->header.e_phnum);
+        printf("\te_shentsize: %d\n", elf->header.e_shentsize);
+        printf("\te_shnum: %d\n", elf->header.e_shnum);
+        printf("\te_shstrndx: %d\n", elf->header.e_shstrndx);
+    }
+    if (flags & 0b10)
+    {
+        printf("ELF segments:\n");
+        for (size_t i = 0; i < elf->header.e_phnum; i++)
+        {
+            printf("\tsegment:\n");
+            printf("\t\tp_type 0x %x\n", elf->segments[i].p_type);
+            printf("\t\tp_flags 0x%x\n", elf->segments[i].p_flags);
+            printf("\t\tp_offset %ld\n", elf->segments[i].p_offset);
+            printf("\t\tp_vaddr 0x%lx\n", elf->segments[i].p_vaddr);
+            printf("\t\tp_paddr 0x%lx\n", elf->segments[i].p_paddr);
+            printf("\t\tp_filesz %ld\n", elf->segments[i].p_filesz);
+            printf("\t\tp_memsz %ld\n", elf->segments[i].p_memsz);
+            printf("\t\tp_align %ld\n", elf->segments[i].p_align);
+        }
+    }
+    if (flags & 0b001)
+    {
+        printf("ELF sections:\n");
+        for (size_t i = 0; i < elf->header.e_shnum; i++)
+        {
+            printf("\tsection:\n");
+            printf("\t\tsh_name: %d\n", elf->sections[i].sh_name);
+            printf("\t\tsh_type: 0x%x\n", elf->sections[i].sh_type);
+            printf("\t\tsh_flags: 0x%lx\n", elf->sections[i].sh_flags);
+            printf("\t\tsh_addr: 0x%lx\n", elf->sections[i].sh_addr);
+            printf("\t\tsh_offset: %ld\n", elf->sections[i].sh_offset);
+            printf("\t\tsh_size: %ld\n", elf->sections[i].sh_size);
+            printf("\t\tsh_link: 0x%x\n", elf->sections[i].sh_link);
+            printf("\t\tsh_info: 0x%x\n", elf->sections[i].sh_info);
+            printf("\t\tsh_addralign: 0x%lx\n", elf->sections[i].sh_addralign);
+            printf("\t\tsh_entsize: %ld\n", elf->sections[i].sh_entsize);
+        }
+    }
 }
 
 int main(int ac, char **av)
@@ -139,6 +193,12 @@ int main(int ac, char **av)
     memset(&elf, 0, sizeof(elf64_t));
     if (elf64_parse(path, &elf) < 0)
         return ft_error("woody: Failed to parse av[1]. Make sure it does existes and is a valid elf64 file.\n");
-    elf64_print(&elf);
-	return (0);
+
+    elf64_print(&elf, 0b111);
+
+    free(elf.data);
+    free(elf.segments);
+    free(elf.sections);
+	
+    return (0);
 }
