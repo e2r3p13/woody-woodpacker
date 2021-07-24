e772dcaed085baf68649fef995f8066082ae3721
Loïc Buckwell <bccyv@protonmail.com>
2021-07-24 17:53:53 +0200

    implement method that replaces the hardcoded stub addresses / values

--- src/main.c
+++ src/main.c
@@ -6,13 +6,14 @@
 /*   By: bccyv <bccyv@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 15:40:55 by bccyv             #+#    #+#             */
-/*   Updated: 2021/07/24 04:12:49 by bccyv            ###   ########.fr       */
+/*   Updated: 2021/07/24 17:52:42 by bccyv            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <elf.h>
 #include <stdio.h>
 #include <woody.h>
+#include <string.h>
 #include <chacha20.h>
 
 uint8_t stub[] =
@@ -20,31 +21,42 @@ uint8_t stub[] =
 	0xeb, 0xfe
 };
 
+static void prepare_stub(uint64_t oep, uint64_t tsz, t_key key)
+{
+	size_t	key_off = 0;
+	size_t	tsz_off = 0;
+	size_t	jmp_off = 0;
+
+	memcpy(stub + key_off, key, 16);
+	memcpy(stub + jmp_off, &oep, sizeof oep);
+	memcpy(stub + tsz_off, &tsz, sizeof tsz);
+}
+
 int main(int ac, char **av)
 {
 	t_elf		*elf;
 	t_key		key;
 	char        out[] = "woody";
+	uint64_t	tsz = 0; // TODO
 
 	if (ac != 2)
 	{
 		printf("Usage: %s <t_elf executable>\n", av[0]);
 		return (1);
 	}
-
 	if ((elf = elf64_read(av[1])) == NULL)
 		return (1);
-
-	if (elf64_is_already_packed(elf) ||
-		chacha20_keygen(key) < 0 ||
-		elf64_encrypt_section(elf, ".text", key) < 0 ||
-		elf64_inject(elf, stub, sizeof(stub)) < 0 ||
-		elf64_write(elf, out) < 0)
+	if (elf64_is_already_packed(elf) || chacha20_keygen(key) < 0 || elf64_encrypt_section(elf, ".text", key) < 0)
 	{
 		elf64_free(elf);
 		return (1);
 	}
-
+	prepare_stub(elf->header.e_entry, tsz, key);
+	if (elf64_inject(elf, stub, sizeof(stub)) < 0 || elf64_write(elf, out) < 0)
+	{
+		elf64_free(elf);
+		return (1);
+	}
 	elf64_free(elf);
 	return (0);
 }
--- src/stub.s
+++ src/stub.s
@@ -1,12 +1,12 @@
 ; **************************************************************************** ;
 ;                                                                              ;
 ;                                                         :::      ::::::::    ;
-;    unpacker.s                                         :+:      :+:    :+:    ;
+;    stub.s                                             :+:      :+:    :+:    ;
 ;                                                     +:+ +:+         +:+      ;
 ;    By: bccyv <bccyv@student.42.fr>                +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2021/07/21 19:41:02 by bccyv             #+#    #+#              ;
-;    Updated: 2021/07/24 00:43:57 by bccyv            ###   ########.fr        ;
+;    Updated: 2021/07/24 17:46:26 by bccyv            ###   ########.fr        ;
 ;                                                                              ;
 ; **************************************************************************** ;
 
@@ -16,27 +16,49 @@ push rdi
 push rsi
 push rdx
 push rax
+push r9
+push r10
+
+mov r9, 0x42	; store oep (replaced when packed)
+mov r10, 0x42	; store text length
 
 ; add write rights to text section
 
-mov rdi, 0x42	; addr of text section
-mov rsi, 0x42	; size of text section
+mov rdi, r9		; addr of text section
+mov rsi, r10	; size of text section
 mov rdx, 2		; PROT_WRITE
 mov rax, 10 	; sys_mprotect
 syscall
 
 ; decrypt text section
 
+
+
 ; remove write and add execute rights to text section
 
-mov rdi, 0x42	; addr of text section
-mov rsi, 0x42	; size of text section
+mov rdi, r9		; addr of text section
+mov rsi, r10	; size of text section
 mov rdx, 4		; PROT_EXEC
 mov rax, 10		; sys_mprotect
 syscall
 
+; print "Woody\n"
+
+mov rax, 0xa79646f6f57
+push rax
+
+mov rdi, 1		; STDOUT
+mov rsi, rsp	; addr of "WOODY\n"
+mov rdx, 6		; size of "WOODY\n"
+mov rax, 1		; write
+syscall
+
+pop rax
+
 ; restore registers
 
+pop r10
+pop rax			; scratch register, to keep r9 to return
 pop rax
 pop rdx
 pop rsi
@@ -44,5 +66,5 @@ pop rdi
 
 ; jump to OEP
 
-push 0x42		; address of OEP
+push r9			; address of OEP
 ret
